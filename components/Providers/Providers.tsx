918412594b8e71d01bba27fde1d4a8549e424f9f
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-04 21:07:41 +0330

    🦬 next theme

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 import React from 'react';
 import {ThemeProvider} from "next-themes";
+import ThemeDataProvider from "@/context/ThemeDataProvider";
 
 const Providers = ({children}: { children: React.ReactNode }) => {
     return (
         <ThemeProvider enableSystem={true} attribute={"class"}>
-            {children}
+            <ThemeDataProvider>
+                {children}
+            </ThemeDataProvider>
         </ThemeProvider>
     );
 };
