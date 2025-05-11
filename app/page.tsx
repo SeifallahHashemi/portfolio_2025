2abbcfa51ea89d3797506ad9aac3e40897efc1b6
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-11 23:50:22 +0330

    🦓 update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <ThemeModeToggle />
+      <div>
+        <h2 className={'font-iranSans'}>سلام دنیا - ایران سنس</h2>
+        <h2 className={'font-iranYWL'}>سلام دنیا - ایران یکان</h2>
+      </div>
     </div>
   );
 }
