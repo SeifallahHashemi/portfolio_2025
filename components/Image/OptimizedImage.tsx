f9e2e8df009055611f5f0ba4c81643f1fb04f851
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-12 00:28:54 +0330

    🐿️ optimized image component with plaiceholder

---
+++ components/Image/OptimizedImage.tsx
@@ -0,0 +1,38 @@
+import { getImage } from '@/lib/getImage';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react';
+
+type OptimizedImageProps = {
+  src: string;
+  alt: string;
+  priority: boolean;
+  quality: number;
+  sizes: string;
+  className?: string;
+};
+
+const OptimizedImage = async ({
+  src,
+  alt,
+  priority = false,
+  quality = 80,
+  sizes = '(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw',
+  className,
+}: OptimizedImageProps): Promise<React.ReactNode> => {
+  const { base64, img } = await getImage(src);
+  return (
+    <Image
+      {...img}
+      alt={alt}
+      priority={priority}
+      quality={quality}
+      sizes={sizes}
+      className={cn('', className)}
+      placeholder={'blur'}
+      blurDataURL={base64}
+    />
+  );
+};
+
+export default OptimizedImage;
