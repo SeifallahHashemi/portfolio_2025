6f737863590acd7138212d3a95aebdcf8f418f0a
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-12 18:12:41 +0330

    🐼 update: add theme mode toggle

--- components/Global/Header.tsx
+++ components/Global/Header.tsx
@@ -1,3 +1,4 @@
+import { ThemeModeToggle } from '@/components/Theme/ThemeModeToggle';
 import Logo from '@/public/img/logo.png';
 import { Link } from 'next-view-transitions';
 import Image from 'next/image';
@@ -27,11 +28,11 @@ const Header = () => {
   return (
     <header
       className={
-        'px-6 py-6 border-b dark:border-b-zinc-800 border-b-zinc-200 text-sm'
+        'px-6 py-6 border-b dark:border-b-zinc-900 border-b-zinc-200 text-sm z-40'
       }
     >
-      <div className={'max-w-6xl mx-auto flex justify-center items-center'}>
-        <Link href={'/'}>
+      <div className={'max-w-6xl mx-auto flex justify-between items-center'}>
+        <Link href={'/'} className={'rounded-full overflow-clip'}>
           <Image
             src={Logo}
             alt={'Logo'}
@@ -44,6 +45,9 @@ const Header = () => {
         <nav>
           <ul></ul>
         </nav>
+        <div className={'flex items-center space-x-4'}>
+          <ThemeModeToggle />
+        </div>
       </div>
     </header>
   );
