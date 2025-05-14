0587f66a5e4429ad6fb8a9b3695022eaedcf266e
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-14 19:21:50 +0330

    🦓 update: test blurIn component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import BlurIn from '@/components/Animation/BlurIn';
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
-        <h2 className={'font-iranSans'}>سلام دنیا - ایران سنس</h2>
-        <h2 className={'font-iranYWL'}>سلام دنیا - ایران یکان</h2>
+        <BlurIn text={'hello world'} />
       </div>
     </div>
   );
