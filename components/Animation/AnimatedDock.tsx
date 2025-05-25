ba5f9dc025f194cfdd4ebb7d0360566f8016cec8
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-25 16:39:05 +0330

    🦨 update animated dock

--- components/Animation/AnimatedDock.tsx
+++ components/Animation/AnimatedDock.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { motion, useMotionValue } from 'motion/react';
 import React from 'react';
 
@@ -13,6 +14,7 @@ type AnimatedDockProps = {
   items: Items[];
   className?: string;
 };
+
 const AnimatedDock = ({ items, className }: AnimatedDockProps) => {
   const mouseXPosition = useMotionValue(Infinity);
   return (
@@ -23,6 +25,10 @@ const AnimatedDock = ({ items, className }: AnimatedDockProps) => {
       onMouseLeave={() => {
         mouseXPosition.set(Infinity);
       }}
+      className={cn(
+        'fixed bottom-4 left-1/2 -translate-x-1/2 hidden md:flex gap-x-4 rounded-full border border-zinc-200 dark:border-zinc-900',
+        className
+      )}
     ></motion.div>
   );
 };
