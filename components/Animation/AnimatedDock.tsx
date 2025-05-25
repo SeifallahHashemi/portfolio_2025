db633c2fec47e79b4a86e810c26689a5e6a1ac22
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-25 16:01:30 +0330

    🦨 update animated dock

--- components/Animation/AnimatedDock.tsx
+++ components/Animation/AnimatedDock.tsx
@@ -1,7 +1,30 @@
+'use client';
+
+import { motion, useMotionValue } from 'motion/react';
 import React from 'react';
 
-const AnimatedDock = () => {
-  return <div></div>;
+interface Items {
+  title: string;
+  href: string;
+  icon: React.ReactNode;
+}
+
+type AnimatedDockProps = {
+  items: Items[];
+  className?: string;
+};
+const AnimatedDock = ({ items, className }: AnimatedDockProps) => {
+  const mouseXPosition = useMotionValue(Infinity);
+  return (
+    <motion.div
+      onMouseMove={(event) => {
+        mouseXPosition.set(event.pageX);
+      }}
+      onMouseLeave={() => {
+        mouseXPosition.set(Infinity);
+      }}
+    ></motion.div>
+  );
 };
 
 export default AnimatedDock;
