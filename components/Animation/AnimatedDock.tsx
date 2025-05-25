6453783b7dd59ab39ac776459d104b2490bb7432
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-25 22:27:19 +0330

    🍳 update layout.tsx

--- components/Animation/AnimatedDock.tsx
+++ components/Animation/AnimatedDock.tsx
@@ -15,7 +15,6 @@ import React, { Fragment, useRef, useState } from 'react';
 
 interface Items {
   title: string;
-  href: string;
   icon: React.ReactNode;
   divider?: boolean;
 }
@@ -34,23 +33,19 @@ const DockAnimation = () => {
     <AnimatedDock
       items={[
         {
-          href: '/',
           icon: <Home />,
           title: 'Home',
         },
         {
-          href: '/search',
           icon: <Search />,
           title: 'Search',
           divider: true,
         },
         {
-          href: '/notifications',
           icon: <Bell />,
           title: 'Notifications',
         },
         {
-          href: '/profile',
           icon: <User />,
           title: 'Profile',
         },
@@ -70,7 +65,7 @@ const AnimatedDock = ({ items, className }: AnimatedDockProps) => {
         mouseXPosition.set(Infinity);
       }}
       className={cn(
-        'fixed bottom-4 left-1/2 -translate-x-1/2 hidden md:flex justify-center items-center cursor-pointer gap-x-2 rounded-full border border-zinc-200 dark:border-zinc-900 bg-white/10 dark:bg-black/10 backdrop-blur-2xl h-16 py-2 px-4',
+        'fixed bottom-4 left-1/2 -translate-x-1/2 hidden md:flex justify-center items-center gap-x-2 rounded-full border border-zinc-200 dark:border-zinc-900 bg-white/30 dark:bg-black/10 backdrop-blur-2xl h-16 py-2 px-4',
         className
       )}
     >
@@ -90,7 +85,7 @@ const AnimatedDock = ({ items, className }: AnimatedDockProps) => {
   );
 };
 
-const DockIcon = ({ mouseX, icon, title, href }: DockIcon) => {
+const DockIcon = ({ mouseX, icon, title }: DockIcon) => {
   const [isHovered, setIsHovered] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
   const distanceFromMouse = useTransform(mouseX, (val) => {
@@ -108,33 +103,31 @@ const DockIcon = ({ mouseX, icon, title, href }: DockIcon) => {
     damping: 12,
   });
   return (
-    <Link href={href}>
-      <motion.div
-        ref={ref}
-        style={{ width }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        className={cn(
-          'relative flex justify-center items-center aspect-square rounded-full text-black shadow-lg dark:text-white'
+    <motion.div
+      ref={ref}
+      style={{ width }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className={cn(
+        'relative flex justify-center items-center aspect-square rounded-full text-black dark:text-white'
+      )}
+    >
+      <AnimatePresence>
+        {isHovered && (
+          <motion.div
+            initial={{ opacity: 0, y: 10, x: '-50%' }}
+            animate={{ opacity: 1, y: 0, x: '-50%' }}
+            exit={{ opacity: 0, y: 2, x: '-50%' }}
+            className="absolute -top-8 left-1/2 w-fit whitespace-pre rounded-md border border-zinc-200 bg-white/80 px-2 py-0.5 text-xs text-neutral-700 dark:border-zinc-900 dark:bg-neutral-800 dark:text-white"
+          >
+            {title}
+          </motion.div>
         )}
-      >
-        <AnimatePresence>
-          {isHovered && (
-            <motion.div
-              initial={{ opacity: 0, y: 10, x: '-50%' }}
-              animate={{ opacity: 1, y: 0, x: '-50%' }}
-              exit={{ opacity: 0, y: 2, x: '-50%' }}
-              className="absolute -top-8 left-1/2 w-fit whitespace-pre rounded-md border border-zinc-200 bg-white/80 px-2 py-0.5 text-xs text-neutral-700 dark:border-zinc-900 dark:bg-neutral-800 dark:text-white"
-            >
-              {title}
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <div className={'w-10 aspect-square flex justify-center items-center'}>
-          {icon}
-        </div>
-      </motion.div>
-    </Link>
+      </AnimatePresence>
+      <div className={'w-10 aspect-square flex justify-center items-center'}>
+        {icon}
+      </div>
+    </motion.div>
   );
 };
 
