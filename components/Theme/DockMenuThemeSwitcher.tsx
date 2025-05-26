d8518d3840fa8b79d6d851d63c9052972bf086dc
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-26 19:29:44 +0330

    🧀 update: icon size

--- components/Theme/DockMenuThemeSwitcher.tsx
+++ components/Theme/DockMenuThemeSwitcher.tsx
@@ -15,11 +15,11 @@ const DockMenuThemeSwitcher = () => {
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <Sun
-        className="!w-6 !h-6 rotate-0 scale-100 transition-all dark:-rotate-90
+        className="!w-5.5 !h-5.5 rotate-0 scale-100 transition-all dark:-rotate-90
           dark:scale-0"
       />
       <Moon
-        className="absolute !w-6 !h-6 rotate-90 scale-0 transition-all dark:rotate-0
+        className="absolute !w-5.5 !h-5.5 rotate-90 scale-0 transition-all dark:rotate-0
           dark:scale-100"
       />
       <span className="sr-only">Toggle theme</span>
