f3226b87a3fb24ebbd4d57e9e31f1192fe500c47
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-26 14:09:09 +0330

    🥞 add new className

--- components/Theme/ThemeModeToggle.tsx
+++ components/Theme/ThemeModeToggle.tsx
@@ -1,10 +1,9 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import * as React from 'react';
 
 export function ThemeModeToggle() {
   const { setTheme, theme } = useTheme();
@@ -13,7 +12,8 @@ export function ThemeModeToggle() {
     <Button
       variant="outline"
       size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className={'cursor-pointer'}
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <Sun
         className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90
