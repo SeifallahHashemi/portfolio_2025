3e8f6a877d3fbf9e3d7ceaab8741b9bc7215187b
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-27 15:40:13 +0330

    🐇 update animation

--- components/Shared/Social.tsx
+++ components/Shared/Social.tsx
@@ -1,19 +1,48 @@
 import { socialLinks } from '@/components/Data/social';
 import RefLink from '@/components/Shared/RefLink';
+import { motion } from 'motion/react';
 import React from 'react';
 
 const Social = () => {
+  const containerVariants = {
+    animate: {
+      transition: {
+        staggerChildren: 0.2,
+        delayChildren: 0.1,
+      },
+    },
+  };
+  const itemVaraints = {
+    initial: { filter: 'blur(0.5rem)', opacity: 0, translateY: '1rem' },
+    animate: {
+      filter: 'blur(0)',
+      opacity: 1,
+      translateY: '0',
+      transition: { duration: 0.4 },
+    },
+  };
   return (
-    <ul className={''}>
+    <motion.ul
+      className={''}
+      variants={containerVariants}
+      initial={'initial'}
+      animate={'animate'}
+    >
       {socialLinks.map((item) => (
-        <li key={item.id} className={''}>
+        <motion.li
+          key={item.id}
+          className={''}
+          variants={itemVaraints}
+          initial={'initial'}
+          animate={'animate'}
+        >
           <RefLink href={item.url} target={'_blank'}>
             <item.icon />
             {item.name}
           </RefLink>
-        </li>
+        </motion.li>
       ))}
-    </ul>
+    </motion.ul>
   );
 };
 
