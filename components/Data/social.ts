fa676e5489733766004bd1460c7e126abef373f6
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-27 14:48:06 +0330

    🦎 social media data

--- components/Data/social.ts
+++ components/Data/social.ts
@@ -1,4 +1,13 @@
-import { SiGithub } from 'react-icons/si';
+import {
+  SiCodepen,
+  SiDiscord,
+  SiDribbble,
+  SiGithub,
+  SiInstagram,
+  SiLinkedin,
+  SiStackoverflow,
+  SiX,
+} from 'react-icons/si';
 
 export const socialLinks = [
   {
@@ -6,6 +15,55 @@ export const socialLinks = [
     name: 'Github',
     url: 'https://github.com/SeifallahHashemi',
     icon: SiGithub,
+    social: true,
+  },
+  {
+    id: 2,
+    name: 'Instagram',
+    url: 'https://www.instagram.com/sepehr_next.js_developer/',
+    icon: SiInstagram,
+    social: true,
+  },
+  {
+    id: 3,
+    name: 'CodePen',
+    url: 'https://codepen.io/seifallahhashemi',
+    icon: SiCodepen,
+    social: true,
+  },
+  {
+    id: 4,
+    name: 'X',
+    url: 'https://x.com/sepehrhashemi6',
+    icon: SiX,
+    social: true,
+  },
+  {
+    id: 5,
+    name: 'Stack Overflow',
+    url: '',
+    icon: SiStackoverflow,
+    social: true,
+  },
+  {
+    id: 6,
+    name: 'Dribbble',
+    url: '',
+    icon: SiDribbble,
+    social: true,
+  },
+  {
+    id: 7,
+    name: 'Discord',
+    url: '',
+    icon: SiDiscord,
+    social: true,
+  },
+  {
+    id: 8,
+    name: 'LinkedIn',
+    url: '',
+    icon: SiLinkedin,
     social: true,
   },
 ];
