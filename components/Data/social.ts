5aad298c3cbd091fd2ae7a02111ca1d845a06a14
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-27 14:14:04 +0330

    🦎 social media data

---
+++ components/Data/social.ts
@@ -0,0 +1,11 @@
+import { SiGithub } from 'react-icons/si';
+
+export const socialLinks = [
+  {
+    id: 1,
+    name: 'Github',
+    url: 'https://github.com/SeifallahHashemi',
+    icon: SiGithub,
+    social: true,
+  },
+];
