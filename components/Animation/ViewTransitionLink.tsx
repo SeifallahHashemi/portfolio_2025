953efa379915ca13bc726a91a1b4c5805e98e02f
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-27 10:53:46 +0330

    🐈 add className props

--- components/Animation/ViewTransitionLink.tsx
+++ components/Animation/ViewTransitionLink.tsx
@@ -1,21 +1,24 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { useTransitionRouter } from 'next-view-transitions';
 import React from 'react';
 
 interface LinkProps {
   link: string;
+  className: string;
   children?: React.ReactNode;
 }
 
-const ViewTransitionLink = ({ link, children }: LinkProps) => {
+const ViewTransitionLink = ({ link, className, children }: LinkProps) => {
   const router = useTransitionRouter();
   return (
     <a
       href={link}
-      className={
-        'dark:text-white text-zinc-700 font-normal dark:hover:text-portfolio-primary hover:text-zinc-900 duration-300 text-base'
-      }
+      className={cn(
+        'dark:text-white text-zinc-700 font-normal dark:hover:text-portfolio-primary hover:text-zinc-900 duration-300 text-base',
+        className
+      )}
       onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
         e.preventDefault();
         router.push(link, {
