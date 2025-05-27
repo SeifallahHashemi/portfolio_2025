8ee892e48c8b5ca2e4cac6096660d704689ea91e
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-27 10:06:50 +0330

    🐈 create view transition link

---
+++ components/Animation/ViewTransitionLink.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { useTransitionRouter } from 'next-view-transitions';
+import React from 'react';
+
+interface LinkProps {
+  link: string;
+  title: string;
+}
+
+const ViewTransitionLink = ({ link, title }: LinkProps) => {
+  const router = useTransitionRouter();
+  return (
+    <a
+      href={link}
+      className={
+        'dark:text-white text-zinc-700 font-normal dark:hover:text-portfolio-primary hover:text-zinc-900 duration-300 text-base'
+      }
+      onClick={(e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault();
+        router.push(link, {
+          onTransitionReady: slideInOut,
+        });
+      }}
+    >
+      {title}
+    </a>
+  );
+};
+
+function slideInOut() {
+  document.documentElement.animate(
+    [
+      {
+        opacity: 1,
+        transform: 'translate(0, 0)',
+      },
+      {
+        opacity: 0,
+        transform: 'translate(-100px, 0)',
+      },
+    ],
+    {
+      duration: 400,
+      easing: 'cubic-bezier(0.76, 0, 0.24, 1)',
+      fill: 'forwards',
+      pseudoElement: '::view-transition-old(root)',
+    }
+  );
+
+  document.documentElement.animate(
+    [
+      {
+        opacity: 0,
+        transform: 'translate(100px, 0)',
+      },
+      {
+        opacity: 1,
+        transform: 'translate(0, 0)',
+      },
+    ],
+    {
+      duration: 400,
+      easing: 'cubic-bezier(0.76, 0, 0.24, 1)',
+      fill: 'forwards',
+      pseudoElement: '::view-transition-new(root)',
+    }
+  );
+}
+
+export default ViewTransitionLink;
