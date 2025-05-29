687bad225fb22f89cd75ac926d787fda2905dece
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 15:15:08 +0330

    🐧 update: contribution graph

--- components/Pages/ContributionGraph.tsx
+++ components/Pages/ContributionGraph.tsx
@@ -1,20 +1,39 @@
 'use client';
 
+import { github } from '@/components/Data/contribution-graph-theme';
 import { useTheme } from 'next-themes';
 import React, { useEffect, useState } from 'react';
 import GitHubCalendar from 'react-github-calendar';
 
 const ContributionGraph = () => {
+  const username = process.env.NEXT_PUBLIC_GITHUB_USERNAME!;
+
   const { theme, systemTheme } = useTheme();
   const [uniqueTheme, setUniqueTheme] = useState<'light' | 'dark' | undefined>(
-    theme
+    theme as 'light' | 'dark'
   );
 
   useEffect(() => {
-    setUniqueTheme(theme || systemTheme);
+    function isValidTheme(t: string | undefined): t is 'light' | 'dark' {
+      return t === 'light' || t === 'dark';
+    }
+
+    const result = theme || systemTheme;
+    if (isValidTheme(result)) {
+      setUniqueTheme(result);
+    }
+
+    setUniqueTheme(undefined);
   }, [theme, systemTheme]);
 
-  return <GitHubCalendar />;
+  return (
+    <GitHubCalendar
+      theme={github}
+      colorScheme={uniqueTheme}
+      username={username}
+      blockSize={13}
+    />
+  );
 };
 
 export default ContributionGraph;
