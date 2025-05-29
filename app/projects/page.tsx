cde828a74acbff0143ec61b0879b00c490c2b49d
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 19:26:19 +0330

    🍐 create projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProjectsPage = () => {
+  return (
+    <div className={'min-h-screen grid place-content-center'}>projects</div>
+  );
+};
+
+export default ProjectsPage;
