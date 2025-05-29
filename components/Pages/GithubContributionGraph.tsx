a6f7ed1c1d9c254f98a3bb9c8b685374efa40291
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 15:43:49 +0330

    🎳 modify: add animation delay & contribution graph

--- components/Pages/GithubContributionGraph.tsx
+++ components/Pages/GithubContributionGraph.tsx
@@ -1,11 +1,15 @@
 import AnimateSection from '@/components/Animation/AnimateSection';
+import ContributionGraph from '@/components/Pages/ContributionGraph';
 import React from 'react';
 
 const GithubContributionGraph = () => {
   return (
     <section>
-      <AnimateSection>
+      <AnimateSection delay={0.7}>
         <h2 className={'portfolioTextWL'}>نمودار فعالیت گیت هاب:</h2>
+      </AnimateSection>
+      <AnimateSection delay={0.8}>
+        <ContributionGraph />
       </AnimateSection>
     </section>
   );
