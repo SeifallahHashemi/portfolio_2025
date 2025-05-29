177220a337ebdf872e5ef4a467200f0f6e42474b
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 15:37:47 +0330

    🎳 create GithubContributionGraph.tsx

---
+++ components/Pages/GithubContributionGraph.tsx
@@ -0,0 +1,18 @@
+import AnimateSection from '@/components/Animation/AnimateSection';
+import React from 'react';
+
+const GithubContributionGraph = () => {
+  return (
+    <section>
+      <AnimateSection>
+        <h2
+          className={'font-iranYWL text-base text-zinc-800 dark:text-zinc-200'}
+        >
+          نمودار فعالیت گیت هاب:
+        </h2>
+      </AnimateSection>
+    </section>
+  );
+};
+
+export default GithubContributionGraph;
