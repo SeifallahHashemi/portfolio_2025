b3963b6cdc8bcee72af58e2ffd32a91563636aa5
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 23:48:42 +0330

    🍓 create velocity marquee

---
+++ components/Animation/VelocityMarquee.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { motion } from 'motion/react';
+import Image from 'next/image';
+import React from 'react';
+
+const upperMarquee = [
+  '/img/01.svg',
+  '/img/02.svg',
+  '/img/03.svg',
+  '/img/04.svg',
+  '/img/05.svg',
+  '/img/06.svg',
+  '/img/07.svg',
+  '/img/08.svg',
+  '/img/09.svg',
+  '/img/10.svg',
+  '/img/11.svg',
+];
+
+const VelocityMarquee = () => {
+  return (
+    <div className={'flex flex-col gap-y-4'}>
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: '100%' }}
+        transition={{ duration: 60, repeat: Infinity, ease: 'linear' }}
+        className={'flex shrink-0'}
+      >
+        {upperMarquee.map((src, ind) => {
+          return (
+            <Image src={src} alt={'pic'} width={56} height={40} key={ind} />
+          );
+        })}
+      </motion.div>
+    </div>
+  );
+};
+
+export default VelocityMarquee;
