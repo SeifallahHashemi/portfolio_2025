ec898523de7cca6ef4c535fad92e61c85f33f2ae
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 17:32:57 +0330

    🧆 create filtered year button

---
+++ components/Shared/FilteredYearButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import React, { MouseEventHandler } from 'react';
+
+interface ButtonProps {
+  year: string;
+  currentYear: string | undefined;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+const FilteredYearButton = ({ year, currentYear, onClick }: ButtonProps) => {
+  return (
+    <Button
+      variant={'ghost'}
+      onClick={onClick}
+      className={cn('', {
+        '!bg-teal-900': currentYear === year,
+      })}
+      title={`سال فعالیت ${year}`}
+    >
+      {year}
+    </Button>
+  );
+};
+
+export default FilteredYearButton;
