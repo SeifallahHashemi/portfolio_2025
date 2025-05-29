458f75e2e8a009a3437c2a6ab1f1ece9299d5e31
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-05-29 19:25:15 +0330

    🫒 create about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className={'min-h-screen grid place-content-center'}>About Me</div>
+  );
+};
+
+export default AboutPage;
