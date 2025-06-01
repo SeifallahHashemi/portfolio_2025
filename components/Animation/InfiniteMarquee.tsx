5f865c492dc2f2d96776fa7c29f2ed7a1252210b
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 11:24:52 +0330

    🦃 update

--- components/Animation/InfiniteMarquee.tsx
+++ components/Animation/InfiniteMarquee.tsx
@@ -8,7 +8,7 @@ const InfiniteMarquee = () => {
       <AnimateSection delay={0.9}>
         <h2
           className={
-            'font-bold text-lg text-center font-iranYWL  text-zinc-800 dark:text-white'
+            'font-bold text-lg text-center font-iranYWL text-zinc-800 dark:text-white'
           }
         >
           مهارت ها
