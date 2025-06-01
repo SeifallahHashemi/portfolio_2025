dce92c8e148cc522532878b9ab0be670620ae516
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 17:57:13 +0330

    🦜 update: className

--- components/Pages/SpotlightGlowingCard.tsx
+++ components/Pages/SpotlightGlowingCard.tsx
@@ -3,7 +3,13 @@
 import React from 'react';
 
 const SpotlightGlowingCard = () => {
-  return <div className={''}></div>;
+  return (
+    <div
+      className={
+        'w-full xl:max-w-6xl mx-auto flex flex-wrap gap-12 items-center justify-center'
+      }
+    ></div>
+  );
 };
 
 export default SpotlightGlowingCard;
