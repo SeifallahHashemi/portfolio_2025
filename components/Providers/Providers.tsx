36e81548c21c0e2878042a8ba0023d4d1d9b4a30
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 10:48:18 +0330

    🥬 add motion lenis provider

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -1,16 +1,22 @@
 'use client';
-import React from 'react';
-import {ThemeProvider} from "next-themes";
-import ThemeDataProvider from "@/context/ThemeDataProvider";
 
-const Providers = ({children}: { children: React.ReactNode }) => {
-    return (
-        <ThemeProvider enableSystem={true} attribute={"class"} defaultTheme={"system"}>
-            <ThemeDataProvider>
-                {children}
-            </ThemeDataProvider>
-        </ThemeProvider>
-    );
+import MotionLenis from '@/components/Providers/MotionLenis';
+import ThemeDataProvider from '@/context/ThemeDataProvider';
+import { ThemeProvider } from 'next-themes';
+import React from 'react';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      enableSystem={true}
+      attribute={'class'}
+      defaultTheme={'system'}
+    >
+      <ThemeDataProvider>
+        <MotionLenis>{children}</MotionLenis>
+      </ThemeDataProvider>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
