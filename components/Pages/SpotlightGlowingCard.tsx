0e6f48ecc0e1c6b7d6ad831b01204a3fb522913d
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 18:19:03 +0330

    🦜 update: create Card component

--- components/Pages/SpotlightGlowingCard.tsx
+++ components/Pages/SpotlightGlowingCard.tsx
@@ -1,6 +1,16 @@
 'use client';
 
+import { Link } from 'next-view-transitions';
 import React from 'react';
+
+interface CardProps {
+  src: string;
+  title: string;
+  text: string;
+  link: string;
+  date: Date;
+  ref: React.Ref<HTMLAnchorElement>;
+}
 
 const SpotlightGlowingCard = () => {
   return (
@@ -9,6 +19,28 @@ const SpotlightGlowingCard = () => {
         'w-full xl:max-w-6xl mx-auto flex flex-wrap gap-12 items-center justify-center'
       }
     ></div>
+  );
+};
+
+const Card = ({ src, title, text, link, date, ref }: CardProps) => {
+  return (
+    <Link
+      href={link}
+      ref={ref}
+      className={
+        'relative h-72 w-96 rounded-xl overflow-clip group bg-white/5 dark:bg-black/5 backdrop-blur-md'
+      }
+    >
+      <div
+        className={
+          'absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-500 pointer-events-none'
+        }
+        style={{
+          background:
+            'radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(51, 26, 52, 0.4), transparent 40%)',
+        }}
+      />
+    </Link>
   );
 };
 
