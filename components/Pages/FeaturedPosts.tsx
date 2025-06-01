0cef721faf712ccc402cceb9ed21400c1583b76b
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 11:12:43 +0330

    🥩 create: featured posts section

---
+++ components/Pages/FeaturedPosts.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FeaturedPosts = () => {
+  return (
+    <div
+      className={
+        'my-20 w-full xl:max-w-6xl mx-auto flex justify-center items-center'
+      }
+    >
+      hello world
+    </div>
+  );
+};
+
+export default FeaturedPosts;
