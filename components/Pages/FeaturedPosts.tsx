583d526fb9cd873ac913a5c3bdaede9f215c77aa
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 11:20:08 +0330

    🥩 update

--- components/Pages/FeaturedPosts.tsx
+++ components/Pages/FeaturedPosts.tsx
@@ -1,14 +1,17 @@
+import AnimateSection from '@/components/Animation/AnimateSection';
 import React from 'react';
 
 const FeaturedPosts = () => {
   return (
-    <div
+    <section
       className={
         'my-20 w-full xl:max-w-6xl mx-auto flex justify-center items-center'
       }
     >
-      hello world
-    </div>
+      <AnimateSection delay={1.1}>
+        <h2>hello world</h2>
+      </AnimateSection>
+    </section>
   );
 };
 
