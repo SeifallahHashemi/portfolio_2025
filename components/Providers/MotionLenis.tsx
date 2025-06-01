7c3704a1aa098528ba735d8d9f9e5ba3e4027693
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-01 10:42:07 +0330

    🦬 motion & lenis provider

---
+++ components/Providers/MotionLenis.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import ReactLenis, { LenisRef } from 'lenis/react';
+import { cancelFrame, frame } from 'motion/react';
+import React, { useEffect, useRef } from 'react';
+
+const MotionLenis = () => {
+  const ref = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp;
+      ref.current?.lenis?.raf(time);
+    }
+    frame.update(update, true);
+
+    return () => cancelFrame(update);
+  }, []);
+  return <ReactLenis root ref={ref} options={{ autoRaf: false }}></ReactLenis>;
+};
+
+export default MotionLenis;
