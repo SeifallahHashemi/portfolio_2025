43c41f9a5c16a599093b077b3fd9d12a9fd4170c
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-02 14:39:30 +0330

    🐄 update 7: finally solved

--- components/Pages/SpotlightGlowingCard.tsx
+++ components/Pages/SpotlightGlowingCard.tsx
@@ -1,75 +1,80 @@
 'use client';
 
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useSpring,
-} from 'motion/react';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const SpotlightGlowingCard = () => {
+  const cardsRef = useRef<HTMLDivElement[]>([]);
+  useEffect(() => {
+    const HandleMouseMove = (e: MouseEvent) => {
+      cardsRef.current.forEach((card) => {
+        if (card) {
+          const rect = card.getBoundingClientRect();
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+          card.style.setProperty('--mouse-x', `${x}px`);
+          card.style.setProperty('--mouse-y', `${y}px`);
+        }
+      });
+    };
+    document.addEventListener('mousemove', HandleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', HandleMouseMove);
+    };
+  }, []);
   return (
     <div
       className={
         'w-full flex flex-wrap gap-10 justify-center items-center group'
       }
     >
-      <Card />
-      <Card />
-      <Card />
+      {Array.from({ length: 3 }).map((_item, ind) => {
+        return (
+          <Card
+            key={ind}
+            ref={(el: HTMLDivElement) =>
+              (cardsRef.current[ind] = el as HTMLDivElement)
+            }
+          />
+        );
+      })}
     </div>
   );
 };
 
-const Card = () => {
+const Card = ({ ref }: { ref: React.Ref<HTMLDivElement> }) => {
   const [isHovered, setIsHovered] = useState(false);
-  const ref = useRef<HTMLDivElement | null>(null);
-  // Motion Value
-  const mouseX = useMotionValue(0);
-  const mouseY = useMotionValue(0);
-  // Spring Value
-  const springX = useSpring(mouseX, { stiffness: 300, damping: 40 });
-  const springY = useSpring(mouseY, { stiffness: 300, damping: 40 });
-  // Motion Template
-  const spotlightBg = useMotionTemplate`radial-gradient(600px circle at ${springX}px ${springY}px, rgba(59,130,246,0.22), transparent 70%)`;
-  const glowBorder = useMotionTemplate`radial-gradient(300px circle at ${springX}px ${springY}px, rgba(59,130,246,0.40), transparent 100%)`;
-  // Handler
-  const MouseHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-    const rect = ref.current?.getBoundingClientRect();
-    if (!rect) return;
-    mouseX.set(e.clientX - rect.left);
-    mouseY.set(e.clientY - rect.top);
-  };
+
   return (
     <div
       ref={ref}
       className={
         'relative overflow-clip rounded-xl bg-slate-800 w-[300px] h-[380px] group'
       }
-      onMouseMove={MouseHandler}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {/* bg glowing */}
-      <motion.div
+      <div
         className={
           'absolute z-30 pointer-events-none inset-0 transition-opacity duration-500'
         }
         style={{
-          background: spotlightBg,
+          background:
+            'radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(59,130,246,0.22), transparent 70%)',
           opacity: isHovered ? 1 : 0,
         }}
-      ></motion.div>
+      ></div>
       {/* border glowing */}
-      <motion.div
+      <div
         className={
           'absolute inset-0 z-10 pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity duration-500'
         }
         style={{
-          background: glowBorder,
+          background:
+            'radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(59,130,246,0.22), transparent 70%)',
         }}
-      ></motion.div>
+      ></div>
       {/* content */}
       <div
         className={
