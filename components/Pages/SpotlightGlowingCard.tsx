2e55b083671699d2b3a22bfa499fd79201fc1ead
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-02 10:47:09 +0330

    🐄 update 1

--- components/Pages/SpotlightGlowingCard.tsx
+++ components/Pages/SpotlightGlowingCard.tsx
@@ -7,9 +7,42 @@ const SpotlightGlowingCard = () => {
   return (
     <div
       className={
-        'w-full xl:max-w-6xl mx-auto flex flex-wrap gap-10 justify-center items-center'
+        'w-full xl:max-w-6xl mx-auto flex flex-wrap gap-10 justify-center items-center group'
       }
-    ></div>
+    >
+      <Card />
+    </div>
+  );
+};
+
+const Card = () => {
+  return (
+    <div
+      className={
+        'relative overflow-clip rounded-xl bg-slate-800 w-[350px] h-[380px] '
+      }
+    >
+      {/* bg glowing */}
+      <motion.div
+        className={
+          'absolute z-30 pointer-events-none inset-0 opacity-0 hover:opacity-100'
+        }
+      ></motion.div>
+      {/* border glowing */}
+      <motion.div
+        className={
+          'absolute inset-0 z-10 pointer-events-none opacity-0 group-hover:opacity-100'
+        }
+      ></motion.div>
+      {/* content */}
+      <div
+        className={
+          'absolute inset-px rounded-xl overflow-clip bg-slate-900 grid place-content-center'
+        }
+      >
+        hello world
+      </div>
+    </div>
   );
 };
 
