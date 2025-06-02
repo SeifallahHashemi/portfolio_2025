adb3d2f3e43a10577955d0ef61472dfd494f87ca
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-02 20:01:12 +0330

    🐄 update

--- components/Pages/SpotlightGlowingCard.tsx
+++ components/Pages/SpotlightGlowingCard.tsx
@@ -32,9 +32,11 @@ const SpotlightGlowingCard = () => {
         return (
           <Card
             key={ind}
-            ref={(el: HTMLDivElement) =>
-              (cardsRef.current[ind] = el as HTMLDivElement)
-            }
+            ref={(el: HTMLDivElement | null) => {
+              if (el) {
+                cardsRef.current[ind] = el;
+              }
+            }}
           />
         );
       })}
@@ -42,14 +44,14 @@ const SpotlightGlowingCard = () => {
   );
 };
 
-const Card = ({ ref }: { ref: React.Ref<HTMLDivElement> }) => {
+const Card = ({ ref }: { ref: React.RefCallback<HTMLDivElement> }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
       ref={ref}
       className={
-        'relative overflow-clip rounded-xl bg-slate-800 w-[300px] h-[380px] group'
+        'relative overflow-clip rounded-xl bg-zinc-800 w-[300px] h-[380px] group'
       }
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
@@ -57,28 +59,28 @@ const Card = ({ ref }: { ref: React.Ref<HTMLDivElement> }) => {
       {/* bg glowing */}
       <div
         className={
-          'absolute z-30 pointer-events-none inset-0 transition-opacity duration-500'
+          'absolute z-30 pointer-events-none inset-0 transition-opacity duration-500 blur-md'
         }
         style={{
           background:
-            'radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(59,130,246,0.22), transparent 70%)',
+            'radial-gradient(300px circle at var(--mouse-x) var(--mouse-y), rgba(148,163,184,0.25), transparent 70%)',
           opacity: isHovered ? 1 : 0,
         }}
       ></div>
       {/* border glowing */}
       <div
         className={
-          'absolute inset-0 z-10 pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity duration-500'
+          'absolute inset-0 z-10 pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity duration-500 blur-md'
         }
         style={{
           background:
-            'radial-gradient(600px circle at var(--mouse-x) var(--mouse-y), rgba(59,130,246,0.22), transparent 70%)',
+            'radial-gradient(300px circle at var(--mouse-x) var(--mouse-y), rgba(148,163,184,0.5), transparent 70%)',
         }}
       ></div>
       {/* content */}
       <div
         className={
-          'absolute inset-1 rounded-xl overflow-clip bg-slate-900 grid place-content-center z-20'
+          'absolute inset-0.5 rounded-xl overflow-clip bg-zinc-900 grid place-content-center z-20'
         }
       >
         hello world
