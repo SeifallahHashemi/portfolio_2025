0d3ec804715f09c3f056231171682b3f08ef0d59
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-03 17:27:43 +0330

    🦓 update logo

--- components/Global/Header.tsx
+++ components/Global/Header.tsx
@@ -1,7 +1,7 @@
 import ViewTransitionLink from '@/components/Animation/ViewTransitionLink';
 import MobileMenu from '@/components/Global/MobileMenu';
 import { ThemeModeToggle } from '@/components/Theme/ThemeModeToggle';
-import Logo from '@/public/img/logo.avif';
+import Logo from '@/public/img/logo.webp';
 import { Link } from 'next-view-transitions';
 import Image from 'next/image';
 import React from 'react';
