2235cfa7660d0682eee495a89e90a1c317728630
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-03 16:39:56 +0330

    🐢 update: add logo's

--- components/Global/Footer.tsx
+++ components/Global/Footer.tsx
@@ -1,3 +1,8 @@
+import RefLink from '@/components/Shared/RefLink';
+import DarkNextJs from '@/public/img/Next.js_wordmark_dark.svg';
+import LightNextJs from '@/public/img/Next.js_wordmark_light.svg';
+import Sanity from '@/public/img/sanity.svg';
+import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
@@ -11,13 +16,47 @@ const Footer = () => {
         className={'w-full max-w-6xl mx-auto flex justify-between items-center'}
       >
         <div className={'flex gap-2 items-center'}>
-          <h3 className={'font-iranYWR font-medium text-base'}>
+          <h3 className={'font-iranYWR font-medium text-xs'}>
             فناوری مورد استفاده:
           </h3>
-          <ul className={''}></ul>
+          <ul className={'flex flex-wrap gap-2 items-center'}>
+            <li>
+              <RefLink href={'https://sanity.io'} target={'_blank'}>
+                <Image
+                  src={Sanity}
+                  alt={'sanity logo'}
+                  width={40}
+                  height={40}
+                />
+              </RefLink>
+            </li>
+            <li>
+              <RefLink href={'https://nextjs.org'} target={'_blank'}>
+                <Image
+                  alt={'Next.js Logo'}
+                  src={LightNextJs}
+                  width={40}
+                  height={40}
+                  className={'dark:hidden inline-flex'}
+                />
+                <Image
+                  alt={'Next.js Logo'}
+                  src={DarkNextJs}
+                  width={40}
+                  height={40}
+                  className={'hidden dark:inline-flex'}
+                />
+              </RefLink>
+            </li>
+            {/*<li>*/}
+            {/*  <RefLink href={''} target={'_blank'}>*/}
+            {/*    <Image alt={'Vercel Logo'} src={''} width={40} height={40} />*/}
+            {/*  </RefLink>*/}
+            {/*</li>*/}
+          </ul>
         </div>
         <div>
-          <small className="text-zinc-500">
+          <small className="text-zinc-500 font-mono text-xs">
             Copyright &copy; Sepehr Dev {new Date().getFullYear()} All rights
             Reserved
           </small>
