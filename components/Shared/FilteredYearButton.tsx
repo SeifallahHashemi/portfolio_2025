e4333d881d234cae6e3e78adb6aa5e1665cd05fb
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-03 11:35:02 +0330

    🧆 update: className

--- components/Shared/FilteredYearButton.tsx
+++ components/Shared/FilteredYearButton.tsx
@@ -13,7 +13,7 @@ const FilteredYearButton = ({ year, currentYear, onClick }: ButtonProps) => {
       variant={'ghost'}
       onClick={onClick}
       className={cn(
-        'font-mono cursor-pointer text-zinc-800 dark:text-zinc-200',
+        'font-mono cursor-pointer text-zinc-800 dark:text-zinc-200 bg-white dark:bg-neutral-900 border border-transparent transition-colors duration-300 hover:border-zinc-200 dark:hover:border-zinc-800',
         currentYear === year && 'dark:!bg-teal-900 bg-teal-500'
       )}
       title={`سال فعالیت ${year}`}
