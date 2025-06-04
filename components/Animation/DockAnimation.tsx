b8ddfe8d87ebda5c602c553c77486d5ad148ddcd
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-04 18:52:39 +0330

    🦨 update remove next-view-transition DockAnimation.tsx

--- components/Animation/DockAnimation.tsx
+++ components/Animation/DockAnimation.tsx
@@ -15,7 +15,8 @@ import {
   useSpring,
   useTransform,
 } from 'motion/react';
-import { Link } from 'next-view-transitions';
+// import { Link } from 'next-view-transitions';
+import Link from 'next/link';
 import React, { Fragment, useRef, useState } from 'react';
 
 interface Items {
