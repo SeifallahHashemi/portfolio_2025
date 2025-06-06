b784ea1e04f70399750c8dfe7192cf547b1a0418
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-06 22:25:17 +0330

    🪇 test spring animation: v1

--- components/Animation/TwistCard.tsx
+++ components/Animation/TwistCard.tsx
@@ -5,10 +5,31 @@ import { motion } from 'motion/react';
 import Image from 'next/image';
 import React from 'react';
 
+const variants = {
+  initial: {
+    rotateY: 0,
+  },
+  animate: {
+    rotateY: 90,
+  },
+};
+
 const TwistCard = () => {
   return (
-    <motion.div>
-      <Image src={TeamPop} alt={'Team Pop'} width={150} height={150} />
+    <motion.div
+      variants={variants}
+      initial={'initial'}
+      animate={'animate'}
+      transition={{
+        duration: 30,
+        type: 'spring',
+        stiffness: 120,
+        damping: 14,
+        mass: 0.9,
+      }}
+      className={'max-w-fit max-h-fit bg-zinc-100 rounded-full'}
+    >
+      <Image src={TeamPop} alt={'Team Pop'} width={300} height={300} />
     </motion.div>
   );
 };
