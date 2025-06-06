e6b196e4a46fa7560cd171fe7d7111714494b0f8
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-06 23:39:21 +0330

    🪇 test TwistCard Animation: v1.5

--- components/Animation/TwistCard.tsx
+++ components/Animation/TwistCard.tsx
@@ -1,34 +1,74 @@
 'use client';
 
+import LogoPop from '@/public/img/pop-1.png';
 import TeamPop from '@/public/img/pop.png';
-import { motion } from 'motion/react';
+import { motion, useAnimation } from 'motion/react';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const variants = {
   initial: {
     rotateY: 0,
   },
-  animate: {
+};
+const hiddenVariants = {
+  initial: {
     rotateY: 90,
   },
 };
 
 const TwistCard = () => {
+  const teamPopControls = useAnimation();
+  const logoPopControls = useAnimation();
+
+  useEffect(() => {
+    const sequence = async () => {
+      await teamPopControls.start({
+        rotateY: 90,
+      });
+      await logoPopControls.start({
+        rotateY: 0,
+      });
+    };
+
+    sequence();
+  }, [teamPopControls, logoPopControls]);
   return (
-    <motion.div
-      variants={variants}
-      initial={'initial'}
-      animate={'animate'}
-      transition={{
-        duration: 0.5,
-        type: 'tween',
-        ease: 'linear',
-      }}
-      className={'max-w-fit max-h-fit bg-zinc-100 rounded-full'}
+    <div
+      className={'w-screen h-screen flex justify-center items-center relative'}
     >
-      <Image src={TeamPop} alt={'Team Pop'} width={300} height={300} />
-    </motion.div>
+      <motion.div
+        variants={variants}
+        initial={'initial'}
+        animate={teamPopControls}
+        transition={{
+          duration: 0.5,
+          type: 'tween',
+          ease: 'linear',
+          delay: 0.5,
+        }}
+        className={
+          'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+        }
+      >
+        <Image src={TeamPop} alt={'Team Pop'} width={300} height={300} />
+      </motion.div>
+      <motion.div
+        variants={hiddenVariants}
+        initial={'initial'}
+        animate={logoPopControls}
+        transition={{
+          duration: 0.5,
+          type: 'tween',
+          ease: 'linear',
+        }}
+        className={
+          'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+        }
+      >
+        <Image src={LogoPop} alt={'Team Pop'} width={300} height={300} />
+      </motion.div>
+    </div>
   );
 };
 
