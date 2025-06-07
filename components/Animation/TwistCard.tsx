b99616bc97d2fe3cdc585e1f1d44674758798ef4
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-07 12:00:03 +0330

    🪇 test TwistCard Animation: v1.7

--- components/Animation/TwistCard.tsx
+++ components/Animation/TwistCard.tsx
@@ -4,59 +4,89 @@ import LogoPop from '@/public/img/pop-1.png';
 import TeamPop from '@/public/img/pop.png';
 import { motion, useAnimation } from 'motion/react';
 import Image from 'next/image';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const variants = {
-  initial: {
-    rotateY: 0,
+interface TCard {
+  id: number;
+  frontImage: string | StaticImageData;
+  backImage: string | StaticImageData;
+  alt: string;
+}
+
+const Cards: TCard[] = [
+  {
+    id: 1,
+    frontImage: TeamPop,
+    backImage: LogoPop,
+    alt: 'popularity football club image: barcelona',
   },
-};
-const hiddenVariants = {
-  initial: {
-    rotateY: 90,
+  {
+    id: 1,
+    frontImage: LogoPop,
+    backImage: TeamPop,
+    alt: 'popularity football club image: barcelona',
   },
-};
+];
 
 const TwistCard = () => {
-  const teamPopControls = useAnimation();
-  const logoPopControls = useAnimation();
+  const [activeCardInd, setActiveCardInd] = useState<number>(0);
+  const frontControl = useAnimation();
+  const backControl = useAnimation();
 
   useEffect(() => {
-    const sequence = async () => {
-      await teamPopControls.start({
+    const timer = setTimeout(async () => {
+      await frontControl.start({
         rotateY: 90,
       });
-      await logoPopControls.start({
+      await backControl.start({
         rotateY: 0,
       });
-    };
+      setActiveCardInd(1);
+    }, 2000);
 
-    sequence();
-  }, [teamPopControls, logoPopControls]);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [frontControl, backControl]);
+
+  // useEffect(() => {
+  //   const sequence = async () => {
+  //     await teamPopControls.start({
+  //       rotateY: 90,
+  //     });
+  //     await logoPopControls.start({
+  //       rotateY: 0,
+  //     });
+  //   };
+  //
+  //   sequence();
+  // }, [teamPopControls, logoPopControls]);
   return (
     <div
       className={'w-screen h-screen flex justify-center items-center relative'}
     >
       <motion.div
-        variants={variants}
-        initial={'initial'}
-        animate={teamPopControls}
+        initial={{ rotateY: 0 }}
+        animate={frontControl}
         transition={{
           duration: 0.5,
           type: 'tween',
           ease: 'linear',
-          delay: 0.5,
         }}
         className={
           'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
         }
       >
-        <Image src={TeamPop} alt={'Team Pop'} width={300} height={300} />
+        <Image
+          src={Cards[activeCardInd].frontImage}
+          alt={'Team Pop'}
+          width={300}
+          height={300}
+        />
       </motion.div>
       <motion.div
-        variants={hiddenVariants}
         initial={'initial'}
-        animate={logoPopControls}
+        animate={backControl}
         transition={{
           duration: 0.5,
           type: 'tween',
@@ -66,7 +96,12 @@ const TwistCard = () => {
           'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
         }
       >
-        <Image src={LogoPop} alt={'Team Pop'} width={300} height={300} />
+        <Image
+          src={Cards[activeCardInd].backImage}
+          alt={'Team Pop'}
+          width={300}
+          height={300}
+        />
       </motion.div>
     </div>
   );
