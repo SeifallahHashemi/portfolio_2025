cc85f034a3c0768d8a8e9d71485ab675b9e1da77
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-07 11:27:12 +0330

    🪇 update

--- components/Animation/TwistCard.tsx
+++ components/Animation/TwistCard.tsx
@@ -2,72 +2,62 @@
 
 import LogoPop from '@/public/img/pop-1.png';
 import TeamPop from '@/public/img/pop.png';
-import { motion, useAnimation } from 'motion/react';
+import { motion } from 'motion/react';
 import Image from 'next/image';
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const variants = {
-  initial: {
+const initVariants = {
+  show: {
     rotateY: 0,
   },
-};
-const hiddenVariants = {
-  initial: {
+  hide: {
     rotateY: 90,
   },
 };
 
+const cards = [
+  {
+    id: 1,
+    src: TeamPop,
+    variants: initVariants,
+    initial: 'show',
+    animate: 'hide',
+    delay: 2,
+  },
+  {
+    id: 2,
+    src: LogoPop,
+    variants: initVariants,
+    initial: 'hide',
+    animate: 'show',
+    delay: 2.5,
+  },
+];
+
 const TwistCard = () => {
-  const teamPopControls = useAnimation();
-  const logoPopControls = useAnimation();
-
-  useEffect(() => {
-    const sequence = async () => {
-      await teamPopControls.start({
-        rotateY: 90,
-      });
-      await logoPopControls.start({
-        rotateY: 0,
-      });
-    };
-
-    sequence();
-  }, [teamPopControls, logoPopControls]);
   return (
     <div
       className={'w-screen h-screen flex justify-center items-center relative'}
     >
-      <motion.div
-        variants={variants}
-        initial={'initial'}
-        animate={teamPopControls}
-        transition={{
-          duration: 0.5,
-          type: 'tween',
-          ease: 'linear',
-          delay: 0.5,
-        }}
-        className={
-          'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
-        }
-      >
-        <Image src={TeamPop} alt={'Team Pop'} width={300} height={300} />
-      </motion.div>
-      <motion.div
-        variants={hiddenVariants}
-        initial={'initial'}
-        animate={logoPopControls}
-        transition={{
-          duration: 0.5,
-          type: 'tween',
-          ease: 'linear',
-        }}
-        className={
-          'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
-        }
-      >
-        <Image src={LogoPop} alt={'Team Pop'} width={300} height={300} />
-      </motion.div>
+      {cards.map((card) => (
+        <motion.div
+          key={card.id}
+          variants={card.variants}
+          initial={card.initial}
+          animate={card.animate}
+          transition={{
+            duration: 0.5,
+            type: 'tween',
+            ease: 'linear',
+            delay: card.delay,
+          }}
+          className={
+            'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+          }
+        >
+          <Image src={card.src} alt={'Team Pop'} width={300} height={300} />
+        </motion.div>
+      ))}
     </div>
   );
 };
