55c02be6575b9f4eaa42caf51c88b39bd6b73c3e
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-08 21:07:00 +0330

    ⚱️ update v1.1: solved multiple issue with pos sticky

--- components/Animation/SandBar.tsx
+++ components/Animation/SandBar.tsx
@@ -5,14 +5,14 @@ import React from 'react';
 
 const SandBar = () => {
   return (
-    <div className={'sticky top-10 left-20 mb-20 w-fit'}>
+    <div className={'absolute inset-0 flex justify-end'}>
       <motion.div
         className={
-          'relative w-2 h-32 rounded-xl bg-white/50 dark:bg-black/50 backdrop-blur-2xl overflow-clip'
+          'sticky top-4 left-10 w-8 h-60 rounded-4xl bg-white/50 dark:bg-black/10 backdrop-blur-3xl border border-zinc-100 dark:border-zinc-900 overflow-clip'
         }
       >
         <motion.div
-          className={'absolute top-0 left-0 w-full h-full bg-rose-950'}
+          className={'absolute top-0 left-0 w-full h-full bg-violet-950'}
         ></motion.div>
       </motion.div>
     </div>
