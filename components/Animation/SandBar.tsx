346cba40ebf5f414384f345ee70ae46c2a8f1cde
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-08 20:09:20 +0330

    ⚱️ update v1.1: add new div's to shape an hourglass

--- components/Animation/SandBar.tsx
+++ components/Animation/SandBar.tsx
@@ -4,7 +4,19 @@ import { motion } from 'motion/react';
 import React from 'react';
 
 const SandBar = () => {
-  return <motion.div className={'sticky top-10 left-20 mb-20'}></motion.div>;
+  return (
+    <div className={'sticky top-10 left-20 mb-20 w-fit'}>
+      <motion.div
+        className={
+          'relative w-2 h-32 rounded-xl bg-white/50 dark:bg-black/50 backdrop-blur-2xl overflow-clip'
+        }
+      >
+        <motion.div
+          className={'absolute top-0 left-0 w-full h-full bg-rose-950'}
+        ></motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default SandBar;
