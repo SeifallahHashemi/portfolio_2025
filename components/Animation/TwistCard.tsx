d7b669dfb93629f456564390ac42f515a9f73538
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-08 10:33:11 +0330

    🪇 test TwistCard Animation: v1.9

--- components/Animation/TwistCard.tsx
+++ components/Animation/TwistCard.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { sleep } from '@/lib/utils';
 import LogoPop from '@/public/img/pop-1.png';
 import TeamPop from '@/public/img/pop.png';
-import { motion, useAnimation } from 'motion/react';
+import { motion, useAnimate } from 'motion/react';
 import Image, { StaticImageData } from 'next/image';
 import React, { useEffect, useState } from 'react';
 
@@ -30,76 +29,33 @@ const cards: TCard[] = [
 ];
 
 const TwistCard = () => {
+  const [scope, animate] = useAnimate();
   const [activeCardInd, setActiveCardInd] = useState<number>(0);
-  const frontControl = useAnimation();
-  const backControl = useAnimation();
 
   useEffect(() => {
-    const sequence = async () => {
-      if (activeCardInd === 0) {
-        await frontControl.start({
-          rotateY: 0,
-        });
-        await sleep(2000);
-        await frontControl.start({
-          rotateY: 90,
-        });
-        await backControl.start({
-          rotateY: 0,
-        });
+    let isMounted = true;
+    const loop = async () => {
+      while (isMounted) {
+        await animate(
+          'div:first-child',
+          { rotateY: 90 },
+          { duration: 0.5, type: 'tween', ease: 'linear' }
+        );
       }
-      if (activeCardInd !== 0 && activeCardInd % 2 === 0) {
-        await frontControl.start({
-          rotateY: 90,
-        });
-        await backControl.start({
-          rotateY: 0,
-        });
-      }
-      if (activeCardInd !== 0 && activeCardInd % 2 !== 0) {
-        await frontControl.start({
-          rotateY: 0,
-        });
-        await backControl.start({
-          rotateY: 90,
-        });
-      }
-
-      setActiveCardInd((prevState) => {
-        if (prevState === cards.length - 1) {
-          return 0;
-        }
-        return prevState + 1;
-      });
-      setTimeout(sequence, 2000);
     };
-    sequence();
-  }, [frontControl, backControl, activeCardInd]);
+    loop();
+    return () => {
+      isMounted = false;
+    };
+  }, [animate]);
 
-  // useEffect(() => {
-  //   const sequence = async () => {
-  //     await teamPopControls.start({
-  //       rotateY: 90,
-  //     });
-  //     await logoPopControls.start({
-  //       rotateY: 0,
-  //     });
-  //   };
-  //
-  //   sequence();
-  // }, [teamPopControls, logoPopControls]);
   return (
     <div
+      ref={scope}
       className={'w-screen h-screen flex justify-center items-center relative'}
     >
       <motion.div
-        initial={{ rotateY: 90 }}
-        animate={frontControl}
-        transition={{
-          duration: 0.5,
-          type: 'tween',
-          ease: 'linear',
-        }}
+        initial={{ rotateY: 0 }}
         className={
           'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
         }
@@ -112,15 +68,7 @@ const TwistCard = () => {
         />
       </motion.div>
       <motion.div
-        initial={{
-          rotateY: 90,
-        }}
-        animate={backControl}
-        transition={{
-          duration: 0.5,
-          type: 'tween',
-          ease: 'linear',
-        }}
+        initial={{ rotateY: 0 }}
         className={
           'max-w-fit max-h-fit bg-zinc-100 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
         }
