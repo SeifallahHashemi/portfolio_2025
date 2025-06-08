517a9aaf2c3f3f36a895374cf74384e2e85e1e12
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-08 21:08:21 +0330

    🫒 update: add sandbar, className

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,12 @@
+import SandBar from '@/components/Animation/SandBar';
 import TwistCard from '@/components/Animation/TwistCard';
 import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div className={'min-h-screen'}>
+    <div className={'min-h-screen relative'}>
       <TwistCard />
+      <SandBar />
     </div>
   );
 };
