b6665c9298076d9f6238521ccb2e6373b2b384ff
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-08 22:21:06 +0330

    ⚱️ update: test new way to create hourglass animation

--- components/Animation/SandBar.tsx
+++ components/Animation/SandBar.tsx
@@ -3,23 +3,49 @@
 import { AnimationControls, motion } from 'motion/react';
 import React from 'react';
 
-const SandBar = ({ controls }: { controls: AnimationControls }) => {
+const SandBar = ({
+  controls,
+  containerControls,
+}: {
+  controls: AnimationControls;
+  containerControls: AnimationControls;
+}) => {
+  const variants = {
+    initial: {
+      top: '-100%',
+      scale: 0.8,
+    },
+    animate: {
+      top: '0%',
+      scale: 1,
+    },
+  };
+  const containerVariants = {
+    initial: {
+      scale: 0.8,
+    },
+    animate: {
+      scale: 1,
+    },
+  };
   return (
     <div className={'absolute inset-0 flex justify-end'}>
       <motion.div
+        variants={containerVariants}
+        initial={'initial'}
+        animate={containerControls}
         className={
           'sticky top-4 left-10 w-8 h-60 rounded-4xl bg-white/50 dark:bg-black/10 backdrop-blur-3xl border border-zinc-100 dark:border-zinc-900 overflow-clip'
         }
       >
         <motion.div
-          initial={{
-            top: '-100%',
-          }}
+          variants={variants}
+          initial={'initial'}
           animate={controls}
           transition={{
             duration: 2.5,
-            ease: 'easeInOut',
-            repeat: Infinity,
+            ease: [0.36, 0, 0.64, 1],
+            // repeat: Infinity,
           }}
           className={'absolute left-0 w-full h-full bg-rose-950'}
         ></motion.div>
