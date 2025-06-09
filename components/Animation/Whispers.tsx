e4b928be007e206f5f3e787d6c6ad075cb2d2bca
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-09 11:20:21 +0330

    🦇 update: add base

--- components/Animation/Whispers.tsx
+++ components/Animation/Whispers.tsx
@@ -1,7 +1,18 @@
+'use client';
+
 import React from 'react';
 
-const Whispers = () => {
-  return <div></div>;
+const sentence = [
+  'من سپهر هاشمی',
+  'تیم محبوب: بارسلونا',
+  'فیلم محبوب: ارباب حلقه ها',
+];
+
+interface WhispersProps {
+  activeInd: number;
+}
+const Whispers = ({ activeInd }: WhispersProps) => {
+  return <div>{sentence[activeInd]}</div>;
 };
 
 export default Whispers;
