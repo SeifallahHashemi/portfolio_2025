fc11d44fd589bc4111a663e7bc1aa5d7daedb43a
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-10 20:52:24 +0330

    ⚱️ start change: v2

--- components/Animation/SandBar.tsx
+++ components/Animation/SandBar.tsx
@@ -1,48 +1,10 @@
 'use client';
 
-import { AnimationControls, motion, useAnimate } from 'motion/react';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
-const SandBar = ({
-  containerControls,
-  activeInd,
-}: {
-  containerControls: AnimationControls;
-  activeInd: number;
-}) => {
-  const [scope, animate] = useAnimate();
-  const prevIndex = useRef(0);
-  useEffect(() => {
-    const enterAnimation = async () => {
-      await animate(`:scope > div:nth-child(${prevIndex.current + 1})`, {
-        top: '-100%',
-      });
-      await animate(`:scope > div:nth-child(${activeInd + 1})`, {
-        top: '0%',
-      });
-      prevIndex.current = activeInd;
-    };
-    enterAnimation();
-  }, [activeInd]);
-  const variants = {
-    initial: {
-      top: '-100%',
-    },
-    animate: {
-      top: '0%',
-    },
-  };
-  const containerVariants = {
-    initial: {
-      scaleY: 0.95,
-    },
-    animate: {
-      scaleY: 1,
-    },
-  };
+const SandBar = () => {
   return (
     <div
-      ref={scope}
       className={
         'sticky top-4 left-4 w-4 h-12 rounded-4xl bg-white/50 dark:bg-black/10 backdrop-blur-3xl border border-zinc-100 dark:border-zinc-900 overflow-clip origin-top'
       }
