a08611903ea1d3046546f9ecabc1dcc34bdf1f73
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-10 11:15:25 +0330

    ⚱️ start change: v1.1

--- components/Animation/SandBar.tsx
+++ components/Animation/SandBar.tsx
@@ -27,33 +27,31 @@ const SandBar = ({
     },
   };
   return (
-    <div className={'absolute inset-0 flex justify-end'}>
+    <motion.div
+      variants={containerVariants}
+      initial={'initial'}
+      animate={containerControls}
+      transition={{
+        duration: 2.5,
+        ease: 'linear',
+      }}
+      className={
+        'sticky top-4 left-4 w-4 h-12 rounded-4xl bg-white/50 dark:bg-black/10 backdrop-blur-3xl border border-zinc-100 dark:border-zinc-900 overflow-clip origin-top'
+      }
+    >
       <motion.div
-        variants={containerVariants}
+        variants={variants}
         initial={'initial'}
-        animate={containerControls}
+        animate={controls}
         transition={{
           duration: 2.5,
-          ease: 'linear',
+          ease: [0.36, 0, 0.64, 1],
         }}
         className={
-          'sticky top-4 left-4 w-4 h-12 rounded-4xl bg-white/50 dark:bg-black/10 backdrop-blur-3xl border border-zinc-100 dark:border-zinc-900 overflow-clip origin-top'
+          'absolute left-0 w-full h-full bg-zinc-200 dark:bg-zinc-900 rounded-4xl'
         }
-      >
-        <motion.div
-          variants={variants}
-          initial={'initial'}
-          animate={controls}
-          transition={{
-            duration: 2.5,
-            ease: [0.36, 0, 0.64, 1],
-          }}
-          className={
-            'absolute left-0 w-full h-full bg-zinc-200 dark:bg-zinc-900 rounded-4xl'
-          }
-        ></motion.div>
-      </motion.div>
-    </div>
+      ></motion.div>
+    </motion.div>
   );
 };
 
