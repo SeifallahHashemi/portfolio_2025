811e5cb4baf916c53185113748eefeb939aa07b1
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-11 12:16:08 +0330

    🫒 update: add CodeTypingLoop.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,12 @@
+import CodeTypingLoop from '@/components/Animation/CodeTypingLoop';
 import TwistCard from '@/components/Animation/TwistCard';
 import React from 'react';
 
 const AboutPage = () => {
   return (
-    <div className={'min-h-screen relative'}>
+    <div className={'min-h-screen relative flex flex-col gap-y-20'}>
       <TwistCard />
+      <CodeTypingLoop />
     </div>
   );
 };
