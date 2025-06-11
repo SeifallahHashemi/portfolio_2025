11ef703e8c4c17bf223d373a61db72100f5a863d
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-11 22:13:13 +0330

    🥭 Enhance page performance by adding new conditions and animations

---
+++ components/Pages/InformationAnimations.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import TwistCard from '@/components/Animation/TwistCard';
+import {
+  AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  usePresence,
+  useScroll,
+  useTransform,
+} from 'motion/react';
+import React, { useEffect, useRef, useState } from 'react';
+
+const InformationAnimations = () => {
+  const [isPresent, safeToRemove] = usePresence();
+  const [toggleList, setToggleList] = useState<boolean>(true);
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollY } = useScroll({
+    target: ref,
+    offset: ['start start', 'end end'],
+  });
+  const opacity = useTransform(scrollY, [0, 300], [1, 0]);
+
+  useMotionValueEvent(opacity, 'change', (latest) => {
+    if (latest === 0) {
+      setToggleList(false);
+    } else if (latest > 0) {
+      setToggleList(true);
+    }
+  });
+
+  useEffect(() => {
+    if (!isPresent) {
+      safeToRemove();
+    }
+  }, [isPresent]);
+
+  return (
+    <AnimatePresence>
+      <section className={'h-screen relative'} ref={ref}>
+        {toggleList && (
+          <motion.div
+            key={'toggle-list'}
+            style={{
+              opacity,
+            }}
+          >
+            <TwistCard />
+          </motion.div>
+        )}
+      </section>
+    </AnimatePresence>
+  );
+};
+
+export default InformationAnimations;
