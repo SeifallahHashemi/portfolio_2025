9cd71572179172f46242e5af1ac70e060daea252
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-06-11 22:13:57 +0330

    🫒 update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,9 @@
-import TwistCard from '@/components/Animation/TwistCard';
 import InformationAnimations from '@/components/Pages/InformationAnimations';
 import React from 'react';
 
 const AboutPage = () => {
   return (
     <main className={'flex flex-col'}>
-      {/*<TwistCard />*/}
       <InformationAnimations />
       <section className={'h-screen'}></section>
     </main>
