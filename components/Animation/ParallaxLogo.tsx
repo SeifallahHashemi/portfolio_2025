6620a23c1a801b3864eee217e60106892d9b0b8b
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-08-24 17:58:01 +0330

    🫓 create new infinite marquee (parallax logo) => evolution 3: import logo's

--- components/Animation/ParallaxLogo.tsx
+++ components/Animation/ParallaxLogo.tsx
@@ -1,6 +1,14 @@
 'use client';
 
+import CSS from '@/components/Icons/css';
+import NextJsSvg from '@/components/Icons/next-js-svg';
+import ReactSVG from '@/components/Icons/react-svg';
+import ReduxSvg from '@/components/Icons/redux-svg';
+import TailwindSvg from '@/components/Icons/tailwind-svg';
+import TypescriptSvg from '@/components/Icons/typescript-svg';
 import React from 'react';
+
+const logos = [CSS, NextJsSvg, ReactSVG, ReduxSvg, TailwindSvg, TypescriptSvg];
 
 interface Props {
   className: string;
