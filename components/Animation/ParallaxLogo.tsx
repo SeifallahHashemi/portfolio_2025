63543107099eca6fa9d38a075a3b689c545085ee
SEPEHR <sepehrhashemimamaquani1996@gmail.com>
2025-08-30 17:48:54 +0330

    🫓 create new infinite marquee (parallax logo) => progress 5: enhance ParallaxLogo with motion effects

--- components/Animation/ParallaxLogo.tsx
+++ components/Animation/ParallaxLogo.tsx
@@ -6,6 +6,13 @@ import ReactSVG from '@/components/Icons/react-svg';
 import ReduxSvg from '@/components/Icons/redux-svg';
 import TailwindSvg from '@/components/Icons/tailwind-svg';
 import TypescriptSvg from '@/components/Icons/typescript-svg';
+import {
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+} from 'motion/react';
 import React from 'react';
 
 const logos = [CSS, NextJsSvg, ReactSVG, ReduxSvg, TailwindSvg, TypescriptSvg];
@@ -18,6 +25,16 @@ const ParallaxLogo = ({
   className,
   baseVelocity = 100,
 }: Props): React.ReactElement => {
+  const baseX = useMotionValue<number>(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
   return <div className={className}></div>;
 };
 
