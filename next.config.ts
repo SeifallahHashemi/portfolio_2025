37ea25112d72bfc5dc4d66f5e5fec1fd28a13bd1
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-07 10:45:01 +0330

    🧶 update next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    'http://172.21.0.1:3000',
+    'http://172.21.0.1',
+    'http://localhost:3000',
+  ],
 };
 
 export default nextConfig;
