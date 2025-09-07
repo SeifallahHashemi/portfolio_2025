05c6f0fe84a829e1f4be6d2d2ff2ae332607a6c1
S E P E H R <sepehrhashemimamaquani1996@gmail.com>
2025-09-07 18:55:51 +0330

    🥩 update => progress 1: modify featured posts

--- components/Pages/FeaturedPosts.tsx
+++ components/Pages/FeaturedPosts.tsx
@@ -12,14 +12,11 @@ const FeaturedPosts = () => {
       <AnimateSection
         delay={1.1}
         className={'w-full xl:max-w-6xl mx-auto text-center'}
+        cClassName={
+          'font-bold text-lg text-center font-iranYWL text-zinc-800 dark:text-white leading-relaxed'
+        }
       >
-        <h2
-          className={
-            'font-bold text-lg text-center font-iranYWL text-zinc-800 dark:text-white leading-relaxed'
-          }
-        >
-          پست های ویژه
-        </h2>
+        پست های ویژه
       </AnimateSection>
       <AnimateSection delay={1.2} className={'w-full xl:max-w-6xl mx-auto'}>
         <SpotlightGlowingCard />
